dab3a855b5984b8268ec407af6ce76b0dcff1260
Stellarrr-914 <yusufsholihi1@gmail.com>
2026-03-11 13:42:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -120,22 +120,17 @@ function logout(){
 // ======= DATABASE ANAK =======
 async function tambahPeserta(){
 
-    const input = document
-        .getElementById("inputPeserta")
-        .value;
+    const input = document.getElementById("inputPeserta").value;
 
     const parts = input.split(",");
 
     if(parts.length !== 3){
-
         alert("Format: Nama,Kategori,Level");
-
         return;
-
     }
 
     const nama = parts[0].trim();
-    const kategori = parts[1].trim();
+    const kategori = parseInt(parts[1].trim());
     const level = parts[2].trim();
 
     const urlAPI = "https://script.google.com/macros/s/AKfycbwpp18nPBME5NaMZwKGwOsn1Bj5uJT7a6OH9XpOkeHaqHZdX7gsxZvqA1MVGmnZNMAz/exec";
@@ -148,6 +143,18 @@ async function tambahPeserta(){
             level:level
         })
     });
+
+    // MASUKKAN KE DATABASE LOCAL JUGA
+    databaseAnak.push({
+        nama:nama,
+        kategori:kategori,
+        level:level
+    });
+
+    // UPDATE TABEL TANPA REFRESH
+    tampilAnak();
+
+    document.getElementById("inputPeserta").value = "";
 
     alert("Peserta berhasil ditambah!");
 
@@ -451,5 +458,6 @@ window.onload = function(){
     }
 
 };
+
 
 
