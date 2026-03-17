04ed05c442cfe828e4cbd7d7eb9037778b4ee8a3
Stellarrr-914 <yusufsholihi1@gmail.com>
2026-03-17 09:21:23 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,6 +8,11 @@ const databaseLomba = {};
 // LINK CSV GOOGLE SHEET
 const sheetCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZwz2KR3s-srm3nj-9G1TAJLQg3NJO2_tU98j4KxniklmAnIS_q_kfkgOKGZQ07QYaeZuFWuh018XM/pub?output=csv";
 
+// Tambah di bawah sheetCSV yang lama
+const sheetLombaCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZwz2KR3s-srm3nj-9G1TAJLQg3NJO2_tU98j4KxniklmAnIS_q_kfkgOKGZQ07QYaeZuFWuh018XM/pub?gid=1443453687&single=true&output=csv"; 
+
+// Update databaseLomba biar otomatis baca dari memori browser
+let databaseLomba = JSON.parse(localStorage.getItem("databaseLomba")) || {};
 
 // ======= LOAD DATA SHEET =======
 
@@ -34,31 +39,24 @@ if (typeof tampilAnak === "function") {
 
 }
 
-
 // ======= PARSE CSV =======
 
-function parseCSV(csv){
-
+function parseCSV(csv) {
     databaseAnak = [];
+    // GANTI baris bawah ini biar karakter \r ilang
+    const rows = csv.split(/\r?\n/); 
 
-    const rows = csv.split("\n");
-
-    for(let i=1;i<rows.length;i++){
-
+    for (let i = 1; i < rows.length; i++) {
         const cols = rows[i].split(",");
-
-        if(cols.length < 3) continue;
+        if (cols.length < 3) continue;
 
         databaseAnak.push({
-
             nama: cols[0].trim(),
-            kategori: parseInt(cols[1]),
-            level: cols[2].trim()
-
+            kategori: cols[1].trim(),
+            level: cols[2].trim().toUpperCase() // Sekarang level "A+" aman dari \r
         });
-
     }
-
+}
     console.log("Data peserta:", databaseAnak);
 
 }
